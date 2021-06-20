010ec1d659da680caf9f3611638d42afde629a40
Yu Yuan Liu <liuyy3364@gmail.com>
2021-06-20 18:24:54 +0800

    Update Optimizer.py

--- Optimizer.py
+++ Optimizer.py
@@ -1,1 +1,18 @@
 # 301
+
+class Optimizer:
+    #麻煩 class 名稱第一個字母大寫
+    def __init__(self) -> None:
+        raise Exception("use empty class")
+    
+    def optimize(self) -> None:
+        raise Exception("not implemented")
+
+class Netadapt:
+    # 麻煩 class 名稱第一個字母大寫
+    # 複製後開始修改，function I/O 自己可以定，註釋記得打清楚就好，也可以先問一下大家討論一下
+    def __init__(self)-> None:
+        raise Exception("use empty class")
+    
+    def optimize(self):
+        raise Exception("not implemented")
