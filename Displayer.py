78b46429cbeda7c3228e9848fc2eb8881bd67fe4
Yu Yuan Liu <liuyy3364@gmail.com>
2021-06-20 18:34:15 +0800

    Update template

---
+++ Displayer.py
@@ -0,0 +1,7 @@
+class Displayer:
+    #麻煩 class 名稱第一個字母大寫
+    def __init__(self) -> None:
+        raise Exception("use empty class")
+    
+    def run(self):
+        raise Exception("not implemented")
---
+++ Tracker.py
@@ -0,0 +1,7 @@
+class Tracker:
+    #麻煩 class 名稱第一個字母大寫
+    def __init__(self) -> None:
+        raise Exception("use empty class")
+    
+    def track(self) -> list:
+        raise Exception("not implemented")
