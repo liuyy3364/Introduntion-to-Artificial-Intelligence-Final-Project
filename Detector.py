a79453d6d805ee4bf3ba32c4fceb6c713820871c
Yu Yuan Liu <liuyy3364@gmail.com>
2021-06-20 18:28:04 +0800

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -1,8 +1,9 @@
 import numpy as np
-class Optimizer:
+
+class Detector:
     #麻煩 class 名稱第一個字母大寫
     def __init__(self) -> None:
         raise Exception("use empty class")
     
-    def detect(self, frame: ) -> list:
+    def detect(self, frame: np.ndarray) -> list:
         raise Exception("not implemented")
