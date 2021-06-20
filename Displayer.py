bb4dfe2f9469ef3bd0a42c018485edb167695063
Yu Yuan Liu <liuyy3364@gmail.com>
2021-06-20 19:45:08 +0800

    update template

--- Displayer.py
+++ Displayer.py
@@ -1,4 +1,3 @@
-# 301
 class Displayer:
     #麻煩 class 名稱第一個字母大寫
     def __init__(self) -> None:
@@ -8,8 +7,11 @@ class Displayer:
         raise Exception("use empty class")
 
     def update_frame(self, frame):
+        """
+        function的說明
+        """
+        raise Exception("not implemented")
 
-    
     def run(self):
         """
         function的說明
@@ -18,6 +20,7 @@ class Displayer:
 
 class Flask_Displayer:
     #麻煩 class 名稱第一個字母大寫
+    frame = bytes()
     def __init__(self) -> None:
         """
         function的說明
@@ -25,7 +28,7 @@ class Flask_Displayer:
         raise Exception("use empty class")
 
     def update_frame(self, frame):
-
+        self.frame = frame
     
     def run(self):
         """
