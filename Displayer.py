75af395d59a3bfc22088f7eb89270bfbee3003ee
Yu Yuan Liu <liuyy3364@gmail.com>
2021-06-20 19:42:58 +0800

    301 pickup

--- Displayer.py
+++ Displayer.py
@@ -1,3 +1,4 @@
+# 301
 class Displayer:
     #麻煩 class 名稱第一個字母大寫
     def __init__(self) -> None:
@@ -5,9 +6,30 @@ class Displayer:
         function的說明
         """
         raise Exception("use empty class")
+
+    def update_frame(self, frame):
+
     
     def run(self):
         """
         function的說明
         """
         raise Exception("not implemented")
+
+class Flask_Displayer:
+    #麻煩 class 名稱第一個字母大寫
+    def __init__(self) -> None:
+        """
+        function的說明
+        """
+        raise Exception("use empty class")
+
+    def update_frame(self, frame):
+
+    
+    def run(self):
+        """
+        function的說明
+        """
+        raise Exception("not implemented")
+
