deada0ff2f43be58eebdf6f0480ddce3b774f8d0
Yu Yuan Liu <liuyy3364@gmail.com>
2021-06-21 23:08:01 +0800

    update tracker

--- Tracker.py
+++ Tracker.py
@@ -1,6 +1,6 @@
 #059 base
 import numpy as np
-from sort import *
+from sort.sort import *
 
 class Tracker_Template:
     #麻煩 class 名稱第一個字母大寫
@@ -16,9 +16,13 @@ class Tracker_Template:
         """
         raise Exception("not implemented")
 
+# SORT
+import copy
 def xywh_to_xyxy(bboxes: np.ndarray):
-    for i, (x, y, w, h) in enumerate(bboxes):
-        bboxes[i] = (x, y, x+w, y+h)
+    ret = copy.deepcopy(bboxes)
+    for i, (x, y, w, h) in enumerate(ret):
+        ret[i] = (x, y, x+w, y+h)
+    return ret
 
 class SORT:
     def __init__(self) -> None:
@@ -28,14 +32,13 @@ class SORT:
         # raise Exception("use empty class")
         self.model = Sort(max_age=3, min_hits=1, iou_threshold=0.15)
     
-
     def track(self, track_bbs: np.ndarray) -> list:
         """
         track_bbs: a list of bounded boxes(x, y, w, h)
         ret: a list of ids
         """
         # raise Exception("not implemented")
-        xywh_to_xyxy(track_bbs)
+        persons = xywh_to_xyxy(track_bbs)
         track_bbs_ids = mot_tracker.update(persons)
         ids = []
         for (x, y, w, h, bid) in track_bbs_ids:
